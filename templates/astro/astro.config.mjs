60d983b45f0a0a004b9175661ee39892cf3b0c5c
benminer <bdminer96@gmail.com>
2023-03-17 09:47:47 -0500

    move out templates and examples

--- templates/astro/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "astro/config";
-import ampt from "@ampt/astro";
-import svelte from "@astrojs/svelte";
-
-// https://astro.build/config
-export default defineConfig({
-  output: "server",
-  integrations: [ampt(), svelte()],
-});
--- templates/react/index.ts
+++
@@ -1,3 +0,0 @@
-import { http } from '@ampt/sdk'
-
-http.on(404, 'static/index.html')
